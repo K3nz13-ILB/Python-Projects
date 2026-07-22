dcc82574b280350ef46c203eb4e91b62cc3c05c2
Kenzie Sariputa <ksariputa1b@gmail.com>
2026-07-22 13:51:45 +0800

    Area of circle

--- Learning Python with Kaggle.py
+++ Learning Python with Kaggle.py
@@ -1,2 +1,7 @@
-color = input('What is your favorite color?')
-print(str.capitalize(color), 'is a great color!')
+pi = 3.14159 #approximate
+diameter = float(input('Diameter of the circle: '))
+
+radius =  diameter/2.0
+area = pi * radius**2
+
+print('The area of this circle is ',area)
