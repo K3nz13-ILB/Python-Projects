648972701caa37824c28bb4d4163803088490bc0
Kenzie Sariputa <ksariputa1b@gmail.com>
2026-07-22 13:10:55 +0800

    created a (bad) calculator

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,14 @@
-name = input('Name: ')
-print('Your name is ',name)
+num1 = input('Input first number: ')
+num2 = input('Input second number: ')
+op = input('Input operation(add,sub,div,mul): ')
+
+if op == 'add':
+    sol = float(num1) + float(num2)
+if op == 'sub':
+    sol = float(num1) - float(num2)
+if op == 'div':
+    sol = float(num1) / float(num2)
+if op == 'mul':
+    sol = float(num1) * float(num2)
+
+print(sol)
